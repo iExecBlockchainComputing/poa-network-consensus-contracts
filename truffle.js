dae7a03668229ba4a6d01e396ab45b52897b7cab
Remi Burgel <remi.burgel@iex.ec>
2020-05-27 09:54:47 +0200

    Fixed wrong provider

--- truffle.js
+++ truffle.js
@@ -5,6 +5,7 @@ const path = "./initialKey.private";
 if(fs.existsSync(path)){
   privatekey = fs.readFileSync(path).toString();
 }
+const fakeProvider = new PrivateKeyProvider(privatekey.substring(2), "http://localhost:8545")
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -36,7 +37,9 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     fakeCeremony: {
-      provider: new PrivateKeyProvider(privatekey.substring(2), "http://locahost:8545"),
+      provider: fakeProvider,
+      gas: 6400000,
+      gasPrice: "0",
       network_id: "*" // Match any network id
     }
   },
