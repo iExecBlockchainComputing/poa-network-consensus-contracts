2fd7b97f826e8972247c3f08145e75059b0ef7de
Remi Burgel <remi.burgel@iex.ec>
2020-05-27 10:13:51 +0200

    Key initialised in fakeCeremony

--- fakeCeremony.js
+++ fakeCeremony.js
@@ -4,7 +4,18 @@ const fs = require('fs');
 const KeysManager = artifacts.require("./KeysManager.sol");
 const contracts = require("./contracts.json");
 
-module.exports = async () => {
+
+module.exports = (callback) => {
     let keyManager = KeysManager.at(contracts.KEYS_MANAGER_ADDRESS);
-    console.log(keyManager.address + "hello");
+    let miningKey = Wallet.generate();
+    let votingKey = Wallet.generate();
+    let payoutKey = Wallet.generate();
+    fs.writeFileSync('./miningKey.json', miningKey.toV3String("ceremony"));
+    fs.writeFileSync('./votingKey.json', votingKey.toV3String("ceremony"));
+    fs.writeFileSync('./payoutKey.json', payoutKey.toV3String("ceremony"));
+    keyManager.createKeys.call(miningKey.getAddressString(), votingKey.getAddressString(), payoutKey.getAddressString()).then(() =>{
+        callback()
+    })
+
+
 }
